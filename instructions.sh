d494fc984cbc9089b9db652b45699b22ae462b59
Ubuntu <ubuntu@ip-172-31-11-200.us-east-2.compute.internal>
2023-05-22 02:04:06 +0000

    This is my bash script test

---
+++ instructions.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+
+#OWNER: JODENUSI
+#DATE: MARCH 23 2023
+#DESCRIPTION: Introduction into automation using bash shell scripting. This file will contain various linux commands to test run bash scripting.
+
+
+
+#Greetings from the machine
+echo "Hi there, I hope you are doing well today"
+sleep 5
+clear
+
+#This command is meant to reveal the current user signed to this machine.
+echo "You are the user called"
+whoami
+sleep 5
+clear
+
+#This command reveals the present working directory
+pwd
+
+
+
+
+
+
